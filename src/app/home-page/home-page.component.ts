ed01fe7bc46b68a2da583cc884dfa8d959d8500d
AnnaAlexandrovna <rassvetnavolge1>
2021-02-08 01:49:22 +0300

    start of UI

---
+++ src/app/home-page/home-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngb-carousel-basic',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.css']
+})
+
+export class HomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
