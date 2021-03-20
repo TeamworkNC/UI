160a1e13d1bb1a64ae5120cd53703b6849998787
AnnaAlexandrovna <salatik339@mail.ru>
2021-03-20 23:58:11 +0300

    add ngFor

---
+++ src/app/filmMain.ts
@@ -0,0 +1,14 @@
+export class FilmMain{
+      filmId: number;
+      filmName: string;
+      filmRate: number;
+      filmAge: number;
+      filmImg : string;
+    constructor() {
+       this.filmId = 0;
+       this.filmName = '';
+       this.filmRate = -1;
+       this.filmAge = -1;
+       this.filmImg = '';
+    }
+}
