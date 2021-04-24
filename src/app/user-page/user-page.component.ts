fd8b9469f4741b2b335608eb386ed15e27549b95
AnnaAlexandrovna <salatik339@mail.ru>
2021-04-24 23:29:19 +0300

    add paginator

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -20,6 +20,14 @@ const ELEMENT_DATA1: FilmMain[] = [
   {filmId: 6, filmName: 'Нечто', filmRate: 7.4, filmImg: 'https://upload.wikimedia.org/wikipedia/ru/c/c0/The_thing.jpg', filmAge: 18},
 ];
 
+const ELEMENT_DATA2: FilmMain[] = [
+  {filmId: 1, filmName: 'Омерзительная восьмерка', filmRate: 7.2, filmImg: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/0bf728af-ce48-4c0e-9da3-f20ee81bc276/960x960', filmAge: 18},
+  {filmId: 2, filmName: 'Земля кочевников', filmRate: 7.3, filmImg: 'http://kinohod.ru/o/c3/59/c359170a-76e0-11eb-941e-f50022a07b64.jfif', filmAge: 16},
+  {filmId: 3, filmName: 'Мавританец', filmRate: 6.0, filmImg: 'http://avatars.mds.yandex.net/get-kinopoisk-image/1777765/d5ed4630-56b3-4a09-a4e4-9f76e105c56e/600x900', filmAge: 18},
+  {filmId: 4, filmName: 'Любовное настроение', filmRate: 8.2, filmImg: 'http://avatars.mds.yandex.net/get-kinopoisk-image/1773646/b6161447-8412-4486-82e4-f2be8bd8d616/600x900', filmAge: 18},
+  {filmId: 5, filmName: 'Паразиты', filmRate: 9.2, filmImg: 'http://avatars.mds.yandex.net/get-kinopoisk-image/1599028/bbe02758-fec4-498f-a128-347ad15dc76c/600x900', filmAge: 18},
+];
+
 export interface PeriodicElement {
   name: string;
   position: number;
@@ -50,10 +58,10 @@ userId1 : number;
 private subscription: Subscription;
 displayedColumns: string[] = ['position', 'name', 'weight'];
 dataSource1 = ELEMENT_DATA1;
-dataSource2 = ELEMENT_DATA1;
+dataSource2 = ELEMENT_DATA2;
 dataSource = ELEMENT_DATA;
-@ViewChild(MatPaginator) paginator: MatPaginator;
-@ViewChild(MatPaginator) paginatorRec: MatPaginator;
+@ViewChild('MatPaginator1') paginator: MatPaginator;
+@ViewChild('MatPaginator2') paginatorRec: MatPaginator;
 obs: Observable<any>;
 dataSource3: MatTableDataSource<FilmMain> = new MatTableDataSource<FilmMain>(this.dataSource1);
 obsRec: Observable<any>;
@@ -191,8 +199,8 @@ close(){
 
 
 @Component({
-  selector: 'failRegistration',
-  templateUrl: 'failRegistration.html',
+  selector: 'failChangeInfo',
+  templateUrl: 'failChangeInfo.html',
 })
 export class FailRegistration {
 constructor(public dialogRef: MatDialogRef<FailRegistration>) {
