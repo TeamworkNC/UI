a963000a0b797a4498460426a01b508187c9e8e4
AnnaAlexandrovna <salatik339@mail.ru>
2021-04-25 01:17:57 +0300

    add paginator

--- src/app/user-page/user-page.component.ts
+++ src/app/user-page/user-page.component.ts
@@ -30,21 +30,17 @@ const ELEMENT_DATA2: FilmMain[] = [
 
 export interface PeriodicElement {
   name: string;
-  position: number;
+  position: string;
   weight: number;
-  symbol: string;
 }
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: "ivan123", name: 'Омерзительная восьмерка', weight: 1},
+  {position: "dima2", name: 'Helium', weight: 2},
+];
+
+const ELEMENT_DATA3: PeriodicElement[] = [
+  {position: "ivan123", name: 'Пошли смотреть фильм "Омерзительная восьмерка"', weight: 1},
+  {position: "dima2", name: 'Пошли смотреть фильм "Helium"', weight: 2},
 ];
 
 @Component({
@@ -60,6 +56,7 @@ displayedColumns: string[] = ['position', 'name', 'weight'];
 dataSource1 = ELEMENT_DATA1;
 dataSource2 = ELEMENT_DATA2;
 dataSource = ELEMENT_DATA;
+dataForNotifications = ELEMENT_DATA3;
 @ViewChild('MatPaginator1') paginator: MatPaginator;
 @ViewChild('MatPaginator2') paginatorRec: MatPaginator;
 obs: Observable<any>;
@@ -181,6 +178,10 @@ sendUserData(){
             this.user = data;
             }
           });
+    }
+
+    goToRoom( roomId : number ){
+      console.log(roomId);
     }
 
 }
