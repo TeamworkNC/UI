cac45f1fdc70f24626c6f2e85ef74dcc8f19a93c
AnnaAlexandrovna <salatik339@mail.ru>
2021-04-28 00:44:14 +0300

    start change film page

--- src/app/req/filmAllGet.ts
+++ src/app/req/filmAllGet.ts
@@ -24,19 +24,50 @@ export class FilmAllGet {
 
   getCommand(filmId: number): Observable <FilmAll> {
 
-    return this.http.get(localUrl1 + filmId , httpOptions ).pipe(map(function (i: any) { return {
+    return this.http.get(localUrl1 + filmId , httpOptions ).pipe(map(function (i: any) {
+    let producers = "";
+    for (let pr in i.producers){
+     producers = producers + i.producers[pr].staff_first_name + " " + i.producers[pr].staff_last_name + ", ";
+    }
+    let genre = "";
+    for (let gr in i.genres){
+     genre = genre + i.genres[gr].genre_title + ", ";
+    }
+    let actors = "";
+    for (let ac in i.actors){
+         actors = actors + i.actors[ac].staff_first_name + " " + i.actors[ac].staff_last_name+ ", ";
+        }
+    let rating = i.rating+"";
+    let carusel = [];
+    for (let o in i.screenshots){
+    let imageObject = {"image":"", "thumbImage":""};
+    imageObject.image = i.screenshots[o].screenshot;
+    imageObject.thumbImage = i.screenshots[o].screenshot;
+    carusel.push(imageObject);
+    }
+
+   let reviews = [];
+   for(let r in i.reviews){
+      let reviewObject = {"mark": 0, "text": "" };
+      reviewObject.mark =i.reviews[r].rating_film;
+      reviewObject.text =i.reviews[r].review;
+      reviews.push(reviewObject);
+   }
+   console.log(reviews);
+
+    return {
      id: i.id_film,
      logo: i.film_poster,
      name : i.film_title,
-     genre : i.genres,
-     producer: i.producers,
+     genre : genre[0].toUpperCase() + genre.slice(1).substr(0, genre.length - 3),
+     producer: producers.substr(0, producers.length - 2),
      description: i.description,
-     actors: i.actors ,
-     rating : i.rating,
+     actors: actors.substr(0, actors.length - 2),
+     rating : rating.substr(0,4),
      ageRestrictions : i.ageLimit.title,
      trailerId : i.film_trailer,
-     imageObject : i.screenshots,
-     reviews : i.reviews
+     imageObject : carusel,
+     reviews : reviews
      };}));
   }
 }
