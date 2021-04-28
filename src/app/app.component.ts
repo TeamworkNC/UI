4747d79713d96dd0d24e9d24946df8b8a0b9e45c
AnnaAlexandrovna <salatik339@mail.ru>
2021-04-28 04:04:41 +0300

    modify userfriends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -43,7 +43,7 @@ export class AppComponent {
 
   goFriendsPage(){
                     this.router.navigate(
-                        ['/friends/'+1]);
+                        ['/friends/'+ this.localStorageService.getItem("userId")]);
     }
 
   goOtherUserPage(){
