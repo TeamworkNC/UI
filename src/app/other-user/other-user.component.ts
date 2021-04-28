f7409755ba2b6850299864a90d3cfddb9fcea06e
AnnaAlexandrovna <salatik339@mail.ru>
2021-04-29 00:44:59 +0300

    go to friend page

--- src/app/other-user/other-user.component.ts
+++ src/app/other-user/other-user.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Subject, Subscription, Observable} from 'rxjs';
+import {ActivatedRoute, Router} from "@angular/router";
+import {PeopleGet} from "src/app/req/peopleGet";
+import {OtherUser} from "src/app/other-user";
 
 @Component({
   selector: 'app-other-user',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./other-user.component.scss']
 })
 export class OtherUserComponent implements OnInit {
-
-  constructor() { }
+userId : number;
+userprofile: OtherUser;
+private subscription: Subscription;
+  constructor(private activateRoute: ActivatedRoute, private api: PeopleGet) {
+    this.subscription = new Subscription();
+    this.userId = 0;
+    this.subscription = this.activateRoute.params.subscribe(params => {
+                this.userId = params['id'];
+                this.getUserData(this.userId);
+              });
+  }
 
   ngOnInit(): void {
   }
+  getUserData(userId: number){
 
+        this.api.getCommand(userId)
+            .subscribe((data: OtherUser) => {
+              if( data == null){
+                this.userprofile = {"userId":0, "birthday": "", "description":"", "email": "", "login": "", "logoUrl": "","registrationDate":"" };
+              }else{
+              this.userprofile = data;
+              console.log(this.userprofile);
+              }
+            });}
 }
