db14994e244e31a68d4cc8163f40e712da2d6c62
AnnaAlexandrovna <salatik339@mail.ru>
2021-05-07 04:30:05 +0300

    modify home page

--- src/app/req/filmAllGet.ts
+++ src/app/req/filmAllGet.ts
@@ -7,7 +7,7 @@ import {map} from "rxjs/operators";
 var headers = new HttpHeaders();
 headers.append('Content-Type', 'application/json; charset=utf-8');
 headers.append('Set-Cookie', 'session=eyJhbGciOiJIUzM4NCJ9.eyJzdWIiOiIyMCIsImlhdCI6MTYxOTQ2OTYxNiwiZXhwIjoxNjE5NDc5NjE2fQ.04HIvGCwKGfM3DAg4nRl3XEhIx-WP30mGymloZ6g79yfdiIhPmKDda8g7HHCmwzf; Max-Age=10000000; Expires=Fri, 20-Aug-2021 14:26:56 GMT; Path=/');
-const localUrl1 = 'https://mac21-portal-backend.herokuapp.com/api/v1/films/full/%7BfilmId%7D?filmId=';//поменять потом
+const localUrl1 = 'https://mac21-portal-backend.herokuapp.com/api/v1/films/full/';//поменять потом
 const httpOptions = {
   headers: new HttpHeaders({
     'Content-Type':  'application/json',
