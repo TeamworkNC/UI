83ef6578af21e4e5f0e5f92710548aa35563cb53
weldnor <kolesov.anton.s@gmail.com>
2021-05-24 16:14:23 +0300

    Improve chat

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,23 +1,31 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, Input, OnInit, ViewChild} from '@angular/core';
 import {RxStompService} from '@stomp/ng2-stompjs';
 import {HttpClient} from '@angular/common/http';
 import {FormControl} from '@angular/forms';
 import {Message} from './message.model';
+import {from, Observable} from 'rxjs';
+import {concat, map, mergeMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.scss']
 })
-export class ChatComponent implements OnInit {
+export class ChatComponent implements OnInit, AfterViewChecked {
 
   @Input()
   public chatId: number;
   @Input()
   public userId: number;
   inputMessageForm = new FormControl();
-  messages: Message[];
+
+  @ViewChild('messagesContainer')
+  messagesContainerRef: ElementRef;
+
+  messages: any[] = [];
   private readonly apiUrl = 'https://mac21-portal-backend.herokuapp.com/api/v1/';
+
+  needScroll = false;
 
   constructor(
     private readonly rxStompService: RxStompService,
@@ -25,20 +33,50 @@ export class ChatComponent implements OnInit {
   ) {
   }
 
-  async ngOnInit(): Promise<any> {
-    this.messages = await this.loadMessages();
-    console.log(this.messages);
-
-    this.rxStompService.watch('/topic/chats/' + this.chatId + '/messages').subscribe(value => {
-      const message = JSON.parse(value.body);
-      console.log(message);
-      this.messages.push(message);
+  ngOnInit(): void {
+    this.getMessagesFromRest().pipe(
+      concat(this.getMessagesFromWs())
+    ).subscribe(message => {
+      this.onMessageReceived(message);
     });
   }
 
-  loadMessages(): Promise<Message[]> {
-    return this.http.get<Message[]>(this.apiUrl + 'chats/' + this.chatId + '/messages').toPromise();
+  ngAfterViewChecked(): void {
+    if (this.needScroll) {
+      this.scrollToBottom();
+      this.needScroll = false;
+    }
   }
+
+  getMessagesFromRest(): Observable<Message> {
+    if (this.userId == null) {
+      throw new Error('cant get messages from rest: user is not authorized');
+    }
+
+    return this.http.get<Message[]>(this.apiUrl + 'chats/' + this.chatId + '/messages').pipe(
+      mergeMap((messages: Message[]) => {
+        return from(messages);
+      }),
+    );
+  }
+
+  getMessagesFromWs(): Observable<Message> {
+    if (this.userId == null) {
+      throw new Error('cant get messages from ws: user is not authorized');
+    }
+
+    return this.rxStompService.watch('/topic/chats/' + this.chatId + '/messages').pipe(
+      map(value => JSON.parse(value.body))
+    ) as unknown as Observable<Message>;
+  }
+
+  scrollToBottom(): void {
+    try {
+      this.messagesContainerRef.nativeElement.scrollTop = this.messagesContainerRef.nativeElement.scrollHeight;
+    } catch (err) {
+    }
+  }
+
 
   onSend(): void {
     const topic = '/app/messages/send';
@@ -51,5 +89,12 @@ export class ChatComponent implements OnInit {
     console.log(data);
     this.rxStompService.publish({destination: topic, body: JSON.stringify(data)});
     this.inputMessageForm.setValue('');
+  }
+
+  private onMessageReceived(message: Message): void {
+    this.http.get<any>(`${this.apiUrl}users/${message.userId}`).subscribe(user => {
+      this.messages.push({text: message.text, username: user.login});
+      this.needScroll = true;
+    });
   }
 }
